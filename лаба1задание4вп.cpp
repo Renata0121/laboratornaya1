a6380113d86509f2ecb3046be21654cfa4d77706
Renata0121 <54933531+Renata0121@users.noreply.github.com>
2019-09-07 13:33:12 +0300

    Add files via upload

---
+++ лаба1задание4вп.cpp
@@ -0,0 +1,24 @@
+﻿// лаба1 задание4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	float a, b, s, d, p, m;
+	cout << "Введите два числа";
+	cin >> a >> b;
+	s = a * a + b * b;
+	d = a * a - b * b;
+	p = (a * a) * (b * b);
+	m = (a * a) / (b * b);
+	cout << "s=" << s;
+	cout << "d=" << d;
+	cout << "p=" << p;
+	cout << "m=" << m;
+	return 0;
+}
+
